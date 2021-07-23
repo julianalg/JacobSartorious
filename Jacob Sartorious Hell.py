0c5016d7f4f674c3331c08e7ed41db8890786781
Julian Garcia <69781917+julianagar@users.noreply.github.com>
2021-07-22 21:07:37 -0700

    initial upload

---
+++ Jacob Sartorious Hell.py
@@ -0,0 +1,28 @@
+import webbrowser
+import time
+
+print("Welcome to the Jacob Sartorius Music Video Spam Opener!")
+print("")
+print("For tips regarding the delay, refer to the README.md")
+delay = int(input("How long would you like the delay to be? "))
+
+
+print("In order to stop this code from running, quit the process in your IDE.")
+print("I am not responsible for any damage this creates.")
+runInput = input("Would you like to run this? 'Y' to run, anything else to quit. ")
+
+while runInput == 'y':
+	webbrowser.open('https://www.youtube.com/watch?v=IvxRsDpXPGo')
+	time.sleep(delay)
+	webbrowser.open('https://www.youtube.com/watch?v=X6yXm88fCa4')
+	time.sleep(delay)
+	webbrowser.open('https://www.youtube.com/watch?v=1e_oDS5HwF8')
+	time.sleep(delay)
+	webbrowser.open('https://www.youtube.com/watch?v=yjnEi8gMlIA')
+	time.sleep(delay)
+	webbrowser.open('https://www.youtube.com/watch?v=6OSOVpEeqCc')
+	time.sleep(delay)
+while runInput == 'n':
+	print('well shit')
+	print('check out more projects from the creator here: ')
+	print('julianagar.carrd.co')
